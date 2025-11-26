ed61d3c3ce39f06bfe75562552424fa6c3c39b17
Silvana Saavedra Londoño <silvana.saavedra@udea.edu.co>
2025-11-26 15:02:40 -0500

    Crear los modelos del proyecto

---
+++ src/main/java/com/pagina/Modelos/Perfil.java
@@ -0,0 +1,71 @@
+package com.pagina.Modelos;
+
+import java.util.List;
+
+public class Perfil {
+    private List<Habilidad> habilidades;
+    private String nombre;
+    private String bio;
+    private String experiencia;
+    private String email;
+    private String foto;
+    private String banner;
+
+    // Getters y setters
+    public List<Habilidad> getHabilidades() {
+        return habilidades;
+    }
+
+    public void setHabilidades(List<Habilidad> habilidades) {
+        this.habilidades = habilidades;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getExperiencia() {
+        return experiencia;
+    }
+
+    public void setExperiencia(String experiencia) {
+        this.experiencia = experiencia;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public String getBanner() {
+        return banner;
+    }
+
+    public void setBanner(String banner) {
+        this.banner = banner;
+    }
+}
+
