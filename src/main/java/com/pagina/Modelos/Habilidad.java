4fd0d9e6b978ad21cea2405dae0208f421687ac6
vflorezz <vflz.625@gmail.com>
2025-11-26 20:06:07 -0500

    Documentación de Perfil y Habilidad

--- src/main/java/com/pagina/Modelos/Habilidad.java
+++ src/main/java/com/pagina/Modelos/Habilidad.java
@@ -1,12 +1,28 @@
 package com.pagina.Modelos;
 
+/**
+ * Modelo que representa una habilidad de la persona de la hoja de vida
+ */
 public class Habilidad {
     private String nombre;
 
+
+    /**
+     * Constructor por defecto vacío
+     */
+    public Habilidad(){
+
+    }
+
+    /**
+     * Constructor para la instancia de una habilidad
+     * @param nombre nombre de la habilidad
+     */
     public Habilidad(String nombre) {
         this.nombre = nombre;
     }
 
+    // Getters y setters
     public String getNombre() {
         return nombre;
     }
