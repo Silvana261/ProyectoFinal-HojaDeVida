c7fe604f68eff75a555b290a8bbb5dca9f9029c4
vflorezz <vflz.625@gmail.com>
2025-11-26 19:28:26 -0500

    documentación de IGestorPerfil

--- src/main/java/com/pagina/Interfaces/IGestorPerfil.java
+++ src/main/java/com/pagina/Interfaces/IGestorPerfil.java
@@ -3,17 +3,45 @@ import com.pagina.Modelos.Perfil;
 
 public interface IGestorPerfil {
 
+    /**
+     * Acutailiza la foto de perfil
+     * @param rutaFoto ruta de la nueva foto
+     */
     void actualizarFotoPerfil(String rutaFoto);
 
+    /**
+     * Edita la bio del perfil
+     * @param bio bio nueva
+     */
     void editarBio(String bio);
 
+    /**
+     * Edita el banner del perfil
+     * @param rutaBanner ruta de el nuevo banner
+     */
     void editarBanner(String rutaBanner);
 
+    /**
+     * Edita el nombre del perfil
+     * @param nombre nombre nuevo del perfil
+     */
     void editarNombre(String nombre);
 
+    /**
+     * Edita la experiencia del perfil (de la hoja de vida)
+     * @param experiencia experiencia nueva del perfil
+     */
     void editarExperiencia(String experiencia);
 
+    /**
+     * Edita el email del perfil
+     * @param email email nuevo
+     */
     void editarEmail(String email);
 
+    /**
+     * Pide al repositorio IRepositorioPerfil el perfil completo
+     * @return el perfil completo
+     */
     Perfil obtenerPerfil();
 }
