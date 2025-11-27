a7eabffd62a6931879e319c26aaa90e92af8f16d
vflorezz <vflz.625@gmail.com>
2025-11-26 19:43:30 -0500

    documentación en IRepositorioHabilidad

--- src/main/java/com/pagina/Interfaces/IRepositorioHabilidad.java
+++ src/main/java/com/pagina/Interfaces/IRepositorioHabilidad.java
@@ -2,8 +2,20 @@ package com.pagina.Interfaces;
 import com.pagina.Modelos.Habilidad;
 import java.util.List;
 
-
+/**
+ * Esta interfaz se encarga de la persistencia de las habilidades
+ */
 public interface IRepositorioHabilidad {
+
+    /**
+     * Carga y devuelve la lista de habilidaddes completa desde el archivo JSON
+     * @return la lista de habilidades
+     */
     List<Habilidad> obtener();
+
+    /**
+     * Guarda la lista de habilidades completa en el archivo JSON
+     * @param habilidades la lista de habilidades
+     */
     void guardar(List<Habilidad> habilidades);
 }
